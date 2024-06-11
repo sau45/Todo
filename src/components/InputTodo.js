a08e6d74f8771efacfbd366fdfab9368c1d1ee55
Saurav Bharti <sauravbharti38@gmail.com>
2024-06-11 11:57:37 +0530

    first commit

---
+++ src/components/InputTodo.js
@@ -0,0 +1,43 @@
+import React, { Fragment, useState } from "react";
+
+const InputTodo = () => {
+  const [description, setDescription] = useState("");
+
+  const onSubmitForm = async e => {
+    e.preventDefault();
+    try {
+      const body = { description };
+      const response = await fetch("http://localhost:5000/pern/todos", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body)
+      });
+
+      window.location = "/";
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+  // background: red;
+  // padding: 1rem;
+  // font-size: 2rem;
+  // font-family: monospace;
+  // border-radius: 10px;
+
+  return (
+    <Fragment>
+      <h1 className="text-center mt-5 bg-blue-500 p-4 text-[2rem] font-mono rounded-xl"> Todo List</h1>
+      <form className="d-flex mt-5" onSubmit={onSubmitForm}>
+        <input
+          type="text"
+          className="form-control border-2 p-4 outline-none rounded-t-lg rounded-b-lg "
+          value={description}
+          onChange={e => setDescription(e.target.value)}
+        />
+        <button className="bg-blue-600 shadow-sm rounded-lg rounded-br-lg p-4 ">Add</button>
+      </form>
+    </Fragment>
+  );
+};
+
+export default InputTodo;
---
+++ src/components/ListTodos.js
@@ -0,0 +1,78 @@
+import React, { Fragment, useEffect, useState } from "react";
+
+import EditTodo from "./EditTodo";
+
+const ListTodos = () => {
+  const [todos, setTodos] = useState([]);
+
+  //delete todo function
+
+  const deleteTodo = async id => {
+    try {
+      const deleteTodo = await fetch(`http://localhost:5000/pern/todos/${id}`, {
+        method: "DELETE"
+      });
+
+      setTodos(todos.filter(todo => todo.todo_id !== id));
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  const getTodos = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/pern/todos");
+      const jsonData = await response.json();
+
+      setTodos(jsonData);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  console.log(todos);
+
+  return (
+    <Fragment>
+      {" "}
+      <table class="table mt-5 text-center">
+        <thead >
+          <tr className="flex gap-56 border items-center justify-center p-3">
+            <th>Description</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/*<tr>
+            <td>John</td>
+            <td>Doe</td>
+            <td>john@example.com</td>
+          </tr> */}
+          {todos.map(todo => (
+            <tr key={todo.todo_id} className="border flex gap-20 items-center justify-center p-4 text-[19px] font-mono">
+              <td>{todo.description}</td>
+              <td>
+                <EditTodo todo={todo} />
+              </td>
+              <td>
+                <button
+                  className="btn btn-danger bg-red-500 p-3 rounded-2xl"
+                  onClick={() => deleteTodo(todo.todo_id)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Fragment>
+  );
+};
+
+export default ListTodos;
